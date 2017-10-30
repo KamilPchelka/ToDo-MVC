822de334c43e4e41006c2d1548c90a874f6cb251
siemaeniu500 <siemaeniu500@gmail.com>
2017-10-30 14:00:21 +0100

    create abstract logic for views

---
+++ views.py
@@ -0,0 +1,85 @@
+import abc
+
+@abc.abstractproperty
+class AbstractView:
+
+    def print_error_message(self, error_msg:str):
+        print('\033[91m' + error_msg + '\033[0m')
+
+    @abc.abstractmethod
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self, msg):
+
+        return input(msg)
+
+    @abc.abstractmethod
+    def print_result_output(self):
+        ...
+
+class AddTodoItemView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self):
+        ...
+
+    def print_result_output(self):
+        ...
+
+
+class ModifyItemView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self):
+        ...
+
+    def print_result_output(self):
+        ...
+
+
+class DeleteItemView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self):
+        ...
+
+    def print_result_output(self):
+        ...
+
+class MarkItemAsDoneView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self):
+        ...
+
+    def print_result_output(self):
+        ...
+
+class DisplayItemListView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+
+    def print_result_output(self):
+        ...
+
+class DisplaySpecificItemView(AbstractView):
+
+    def print_view_info(self):
+        ...
+
+    def get_user_input(self):
+        ...
+
+    def print_result_output(self):
+        ...
