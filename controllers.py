2a88938a99ab7f6cb7bf1435f28593dbb03ccdde
siemaeniu500 <siemaeniu500@gmail.com>
2017-11-02 16:54:21 +0100

    implement basic application logic

--- controllers.py
+++ controllers.py
@@ -26,38 +26,73 @@ class Controller():
     def start(self):
         while True:
             os.system('clear')
-            self.root_view.call_get_user_input_event()
+            try:
+                self.root_view.call_get_user_input_event()
+            except IndexError:
+                self.root_view.print_error_message(
+                    'Please type required arguments in correct type and order !')
+            except ValueError:
+                self.root_view.print_error_message('Your input must contain only positive numbers !')
+            except Exception as e:
+                self.root_view.print_error_message(str(e))
 
     def root_view_listener(self, input:str):
+        tasks_list = ToDoItem.get_todo_items_list() #list of all avaible tasks
         if input not in [str(number) for number in range(1, len(views.MENU_OPTION_LIST) +1)]:
             self.root_view.print_error_message('Bad number of option !')
         elif input == '1':
             self.add_todo_item_view.call_get_user_input_event()
         elif input == '2':
-            self.modify_item_view.call_get_user_input_event()
+            self.modify_item_view.call_get_user_input_event(tasks_list)
         elif input == '3':
-            self.delete_item_view.call_get_user_input_event()
+            self.delete_item_view.call_get_user_input_event(tasks_list)
         elif input == '4':
-            self.mark_item_as_done_view.call_get_user_input_event()
+            self.mark_item_as_done_view.call_get_user_input_event(tasks_list)
         elif input == '5':
             self.display_item_list_view.print_result_output(ToDoItem.get_todo_items_list())
         elif input == '6':
-            self.display_specyfic_item_view.call_get_user_input_event()
+            self.display_specyfic_item_view.call_get_user_input_event(tasks_list)
         elif input == '7':
             self.root_view.exit_program('Thank you for using the program !')
 
     def add_todo_item_view_listener(self, user_input:str):
-        try:
-            splitted_user_input = user_input.split(',')
-            print(splitted_user_input)
-            task_name = splitted_user_input[0]
-            task_desc = splitted_user_input[1]
-            task_status = True if splitted_user_input[2].lower() == 'true' else False
-            ToDoItem(task_name, task_desc, task_status)
-            self.add_todo_item_view.print_result_output(task_name, task_desc, task_status)
-        except IndexError:
-            self.add_todo_item_view.print_error_message('Please type required arguments in correct type and order !')
-        except Exception as e:
-            self.add_todo_item_view.print_error_message(str(e))
+        splitted_user_input = user_input.split(',')
+        task_name = splitted_user_input[0]
+        task_desc = splitted_user_input[1]
+        task_status = True if splitted_user_input[2].lower() == 'true' else False
+        ToDoItem(task_name, task_desc, task_status)
+        self.add_todo_item_view.print_result_output(task_name, task_desc, task_status)
+
+    def modify_item_view_listener(self, user_input:str, tasks_list):
+        splitted_user_input = user_input.split(',')
+        cmd = splitted_user_input[0].lower() #one of the following: 'name' , 'desc' that changes TodoItem attributes
+        index = int(splitted_user_input[1])
+        new_value = splitted_user_input[2] # new name or new description of TodoItem
+        if cmd == 'name':
+            tasks_list[index].set_name(new_value)
+        elif cmd == 'desc':
+            tasks_list[index].set_description(new_value)
+        else:
+            raise Exception('Bad command !')
+        self.modify_item_view.print_result_output()
+        ToDoItem.save_todo_items_to_file()
+
+    def delete_item_view_listener(self, user_input:str, tasks_list):
+        todo_item_to_be_removed = tasks_list[int(user_input)]
+        ToDoItem.remove_todo_item(todo_item_to_be_removed)
+        self.delete_item_view.print_result_output(todo_item_to_be_removed.get_name())
+
+    def mark_item_as_done_view_listener(self, user_input:str, tasks_list):
+        todo_item_to_be_marked_as_done = tasks_list[int(user_input)]
+        todo_item_to_be_marked_as_done.set_is_done(True)
+        self.mark_item_as_done_view.print_result_output(todo_item_to_be_marked_as_done.get_name())
+
+    def display_specyfic_item_view_listener(self, user_input:str, tasks_list):
+        specyfic_todo_item = tasks_list[int(user_input)]
+        name = specyfic_todo_item.get_name()
+        desc = specyfic_todo_item.get_description()
+        status = specyfic_todo_item.get_is_done()
+        self.display_specyfic_item_view.print_result_output(name, desc, status)
+
 
 
